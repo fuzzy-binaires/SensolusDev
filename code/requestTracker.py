24b5bad6db52442990e8e2203fea42c78e057fbf
PleekMan <agusanzorena@gmail.com>
2021-11-26 10:20:36 +0100

    Added .env to hide sensitive data

--- code/requestTracker.py
+++ code/requestTracker.py
@@ -1,8 +1,14 @@
 import requests
 import json
+import os
+from os.path import isfile
+from envparse import env
 
-url = "https://stickntrack.sensolus.com/rest/api/v2/devices/WQ9ENJ"
-apiKey = "36ab8421cb10476b9d8cf46cae0b48a9"
+
+env.read_envfile()
+apiKey = env.str('API_KEY')
+deviceSerial = env.str('DEVICE_0')
+
 
 dateObject = {
     "from":{
@@ -15,17 +21,12 @@ dateObject = {
     }
 }
 
-deviceSerial = "WQ9ENJ"
 
 ########
 ########
 
 
-## Example query: https://stickntrack.sensolus.com/rest/api/v2/geozonevisits/WQ9ENJ?from=2021-10-01T00%3A00%3A00Z&to=2021-10-03T00%3A00%3A00Z&reevaluate=true&apiKey=36ab8421cb10476b9d8cf46cae0b48a9
-## from=2021-10-01T00%3A00%3A00Z
 def buildDateStringParam(dates):
-    ##return {"from":"&from=2021-"+str(dates["from"]["month"]) + "-" + str(dates["from"]["day"]).zfill(2) + "T00%3A00%3A00Z",
-        ##"to":"&to=2021-"+str(dates["to"]["month"]) + "-" + str(dates["to"]["day"]).zfill(2) + "T00%3A00%3A00Z"
     return "&from=2021-"+str(dates["from"]["month"]) + "-" + str(dates["from"]["day"]).zfill(2) + "T00%3A00%3A00Z" + "&to=2021-"+str(dates["to"]["month"]) + "-" + str(dates["to"]["day"]).zfill(2) + "T00%3A00%3A00Z"
 
 def getGeozoneActivity(deviceSerial, dates):
@@ -35,18 +36,17 @@ def getGeozoneActivity(deviceSerial, dates):
 
 def sendQuery(otherParams, flags):
     url = "https://stickntrack.sensolus.com/rest/api/v2/"+ otherParams["queryType"] + otherParams["device"]
-    apiKey = "36ab8421cb10476b9d8cf46cae0b48a9"
 
-    querystring = "apiKey="+apiKey + otherParams["timeFrame"] + flags +"&_csrf=a24c8521-aa26-446d-8e65-4f24436bb888%20-H%20%22accept:%20application/json%22"
+    querystring = "apiKey=" + apiKey + otherParams["timeFrame"] + flags +"&_csrf=a24c8521-aa26-446d-8e65-4f24436bb888%20-H%20%22accept:%20application/json%22"
     payload = ""
     headers = {
         'cache-control': "no-cache",
-        'Postman-Token': "c2a5e613-80d5-415b-8905-903580310ff9"
+        #'Postman-Token': "c2a5e613-80d5-415b-8905-903580310ff9"
     }
 
     response = requests.request("GET", url, data=payload, headers=headers, params=querystring)
 
-    print(response.request.url) ## PRINTING THE URL ORIGINALLY CREATED TO SEND THE QUERY
+    #print(response.request.url) ## PRINTING THE URL ORIGINALLY CREATED TO SEND THE QUERY
     print("=================")
     ##print(response.text) ## RAW RESPONSE
     ##print("=================")
@@ -55,6 +55,7 @@ def sendQuery(otherParams, flags):
     data = json.loads(response.text) ## loads RETURNS A DICTIONARY
     ##print('Name of Tracker: ', data['name'])
     
+    ## PRINT SOME OF THE GEOLOCATOIN DATA
     for i in range(len(data)):
         print("Visit " + str(i) + " => " + data[i]["geozoneName"] + " @ " + data[i]["entryTime"])
 
