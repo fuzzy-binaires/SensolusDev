242bc01bedb7d1a43462b539d06c9bfcbb290f56
diego.valverde <diego.valverde.g@gmail.com>
2021-11-27 17:22:44 +0100

    @cosmetic:

--- code/tracker_request.py
+++ code/tracker_request.py
@@ -20,6 +20,7 @@ def initialize():
     else:
         raise Exception('Environment file not found')
 
+
 dateObject = {
     "from": {
         "month": 10,
@@ -47,7 +48,6 @@ def get_geo_zone_list():
         return [entry['name'] for entry in data]
 
 
-
 def build_date_string_param(dates):
     return "&from=2021-" + str(dates["from"]["month"]) + "-" + str(dates["from"]["day"]).zfill(
         2) + "T00%3A00%3A00Z" + "&to=2021-" + str(dates["to"]["month"]) + "-" + str(dates["to"]["day"]).zfill(
@@ -55,15 +55,15 @@ def build_date_string_param(dates):
 
 
 def get_geo_zone_activity(device_serial, dates):
-    geozone_data = {"timeFrame": build_date_string_param(dates), "flags":"&reevaluate=true"}
-    send_query('geozonevisits', device_serial, geozone_data)
+    geozone_data = {"timeFrame": build_date_string_param(dates), "flags": "&reevaluate=true"}
+    return send_query('geozonevisits', device_serial, geozone_data)
 
 
 def send_query(query_type, device_serial, specific_params=None):
     """
     :param query_type: type of query: [ 'geozonevisits', 'geozones' ]
     :param device_serial: serial number of the tracker
-    :param specific_params: dictionary with aditional parameter specific for the query
+    :param specific_params: dictionary with additional parameter specific for the query
     :return: json dictionary with response from the tracker
     """
 
@@ -71,12 +71,13 @@ def send_query(query_type, device_serial, specific_params=None):
 
     # Note that not all commands require the serial
     if device_serial is not None:
-          url += "/" + device_serial
+        url += "/" + device_serial
 
-    params_string = "apiKey=" + apiKey + "&_csrf=a24c8521-aa26-446d-8e65-4f24436bb888%20-H%20%22accept:%20application/json%22"
+    params_string = "apiKey=" + apiKey + \
+                    "&_csrf=a24c8521-aa26-446d-8e65-4f24436bb888%20-H%20%22accept:%20application/json%22"
 
     if specific_params is not None:
-        params_string  += ''.join(str(x) for x in specific_params.values())
+        params_string += ''.join(str(x) for x in specific_params.values())
 
     payload = ""
     headers = {
@@ -87,11 +88,10 @@ def send_query(query_type, device_serial, specific_params=None):
 
     data = json.loads(response.text)
 
-    if 'type' in data and data['type'] in ['error','unknown']:
+    if 'type' in data and data['type'] in ['error', 'unknown']:
         raise Exception('executing command: url: {} data: {} headers: {} params: {} \n\n {}'.format(url, payload,
                                                                                                     headers,
                                                                                                     params_string,
                                                                                                     data['message']))
 
     return data
-
