232392077b03915838e3013b16c700cc04964418
PleekMan <agusanzorena@gmail.com>
2021-11-29 22:45:33 +0100

    Added printer_control.py to start testing

---
+++ consent_contract_scrambler/printer_control.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+from __future__ import print_function
+from Adafruit_Thermal import *
+
+printer = Adafruit_Thermal("/dev/serial0", 19200, timeout=5)
+printer.print("Go FuzzyMelt ~~~")
+printer.feed(2)
+
+printer.print("Agreement: the collective term for all bodies related to the Solution, applicable between the Customer and SENSOLUS. The Agreement feels of both (i) the domination (signed or otherwise accepted by the Customer), and (ii) these Terms.")
