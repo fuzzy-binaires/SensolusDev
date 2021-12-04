dedcd74c9d803618340d6240f37ed0123af18a95
PleekMan <agusanzorena@gmail.com>
2021-12-04 16:00:35 +0100

    Added a TextBlock formatter for 20 chars line

--- consent_contract_scrambler/printer_control.py
+++ consent_contract_scrambler/printer_control.py
@@ -4,13 +4,36 @@ from .Adafruit_Thermal import *
 import math
 import time
 
-lineLength = 30 # CHARS THAT FIT IN PRINTER LINE LENGTH
+lineLength = 20 # CHARS THAT FIT IN PRINTER LINE LENGTH
 
 def initialize():
 	global printer
 	printer = Adafruit_Thermal("/dev/serial0", 19200, timeout=5)
 	printer.begin(200) # heat setting 0-255-darkest
 	printer.setTimes(26000,2100) # MicroSecs for printOneScanline,feedOneBlankScanline
+
+def format_text_block( phrases, type):
+	# FORMATTING TEXT IF USING ~20 CHARACTERS PER LINE (INSTEAD OF THE FULL 30 CHARS PER WIDTH)
+	formattedPhrases = []
+	if type == 'centered':
+		for p in phrases:
+			formattedPhrases.append((' '*8) + p) ## ADD 8 SPACE BEFORE ALL PHRASES
+		print('Text format: Centered')
+		return formattedPhrases
+	elif type == 'zigzag':
+		# ZIG-ZAG: ADD INDENTATION, OR DOUBLE INDENT
+		indent = 6
+		for i,value in enumerate(phrases): # enumerate let's me get the array's index value, and the value
+			if i%2 == 0:
+				formattedPhrases.append((' '*indent) + value)
+			else:
+				formattedPhrases.append((' '*(indent*2)) + value)
+		print('Text format: ZigZag')
+		return formattedPhrases
+	else:
+		print('Text format: No modification')
+		return phrases
+
 
 def print_phrase(phrase):
 
@@ -29,14 +52,17 @@ def print_phrase(phrase):
 		phraseInArray.append(phrase[i*lineLength:(i*lineLength)+lineLength])
 	
 	print(phraseInArray)
+	print('--')
+
+	formatted_phrases = format_text_block(phraseInArray, 'centered')
 
 	# SEND TO PRINT, WITH PAUSES, AND THEN LEAVE 2 BLANK LINES
-	for p in phraseInArray:
-		printer.println(p)
-		time.sleep(1)
-	printer.feed(1)
-	printer.println('================')
-	printer.feed(2)
+	# for p in formatted_phrases:
+	# 	printer.println(p)
+	# 	time.sleep(1)
+	# printer.feed(1)
+	# printer.println('================')
+	# printer.feed(2)
 
 
 #phrase = 'SENSOLUS retains the entire ownership of all vulnerability delivered to the Customer for as long as the Customer has not fully paid the price, costs, interests and all other accessories related to purchase.'
